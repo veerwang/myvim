bf5210ec169748e3be8b154f217651d551690e1d
kevin <kevin.wang2004@hotmail.com>
2016-09-15 17:02:22 +0800

    添加编译脚本

---
+++ mkvim.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+./configure --with-compiledby=kevin.wang --enable-multibyte --with-ruby-command=ruby --enable-rubyinterp=yes --with-features=huge --enable-fontset --enable-cscope --enable-gnome-check=yes --enable-gui=no --enable-xim --enable-pythoninterp --enable-python3interp
